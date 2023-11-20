da32da01040679804ff160704fb752a94e09c429
daniel-dubon-dev <danieldubonxd@gmail.com>
2023-11-20 09:32:33 -0600

    --shadows effects for banner section

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,14 +1,38 @@
-import TitleSection from '@/components/lading-page/title-section'
 import React from 'react'
-
+import Image from 'next/image'
+import TitleSection from '@/components/lading-page/title-section'
+import Banner from '../../../public/appBanner.png'
 const HomePage = () => {
   return (
     <section>
-      <div className='overflow-hidden md:mt-10 items-center gap-4 px-4 sm:flex sm:flex-col sm:px-6 md:justify-center'>
+      <div className='items-center gap-4 overflow-hidden px-4 sm:flex sm:flex-col sm:px-6 md:mt-10 md:justify-center'>
         <TitleSection
-          pill='Your workspaces here'
-          title='All-in-one collaboration and productivity platform'
+          pill='A better workspaces '
+          title='Write, plan, share.
+          With AI at your side.'
+          subheading='Notion is the connected workspace where better, faster work happens.'
         />
+        <div className='mt-6 rounded-xl bg-white bg-gradient-to-r from-primary to-blue-600 p-[2px]  sm:w-[300px] '>
+          <button className='w-full rounded-[10px] bg-background p-2 text-2xl '>
+            Get cypress free
+          </button>
+        </div>
+        <div
+          className=' className="md:mt-[-90px]
+          relative
+          ml-[-50px]
+          mt-[-40px]
+          flex
+          w-[750px]
+          items-center
+          justify-center
+          sm:ml-0
+          sm:w-full'
+        >
+          <Image src={Banner} alt='banner' />
+          {/* //* shadow effect for banner  */}
+          <div className='absolute bottom-0 left-0 right-0 top-[50%] z-10 bg-gradient-to-t dark:from-background '></div>
+        </div>
       </div>
     </section>
   )
