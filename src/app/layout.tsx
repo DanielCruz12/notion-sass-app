c583297e973ad4b5785f11a2316bc6f5fc73e211
daniel-dubon-dev <danieldubonxd@gmail.com>
2024-02-02 23:23:16 -0600

    test2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={twMerge('bg-background', inter.className)}>
-        <ThemeProvider attribute='class' defaultTheme='dark' enableSystem>
+        <ThemeProvider attribute='class ' defaultTheme='dark' enableSystem>
           <AppStateProvider>
             <SupabaseUserProvider>
               {children}
