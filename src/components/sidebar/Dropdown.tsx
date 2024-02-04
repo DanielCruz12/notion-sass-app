834bf17cf91a726a80e47d25ba4bed4de9aaa5f3
daniel-dubon-dev <danieldubonxd@gmail.com>
2024-02-04 16:20:10 -0600

    tooltips

--- src/components/sidebar/Dropdown.tsx
+++ src/components/sidebar/Dropdown.tsx
@@ -1,8 +1,13 @@
+'use client'
+
 /* eslint-disable no-unused-vars */
 import { useAppState } from '@/lib/providers/state-provider'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import { useRouter } from 'next/router'
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
+import { AccordionItem, AccordionTrigger } from '../ui/accordion'
+import clsx from 'clsx'
+import EmojiPicker from '../global/emoji-picker'
 
 type DropDownProps = {
   title: string
@@ -22,21 +27,84 @@ const Dropdown: React.FC<DropDownProps> = ({
   children,
   disabled,
   customIcon,
+  ...props
 }) => {
-    const supabase = createClientComponentClient()
-    const { state, dispatch, workspaceId } = useAppState()
-    const [isEditing, setIsEditing] = useState(false)
-    const router = useRouter()
-    //folder title synced with server data and local
+  const supabase = createClientComponentClient()
+  const { state, dispatch, workspaceId, folderId } = useAppState()
+  const [isEditing, setIsEditing] = useState(false)
+  const router = useRouter()
+
+  const isFolder = listType === 'folder'
+  const groupIdentifies = useMemo(() => {
+    return clsx(
+      'dark:text-white whitespace-nowrap flex justify-between items-center w-full relative',
+      {
+        'group/folder': isFolder,
+        'group/file': !isFolder,
+      }
+    )
+  }, [isFolder])
+  const listStyles = useMemo(() => {
+    return clsx('relative', {
+      'border-none': isFolder,
+      'border-none m-6 text-[16px] py-1': !isFolder,
+    })
+  }, [isFolder])
+  //folder title synced with server data and local
   //fileTitle
   //navigate the user to a different page
+  const navigateToPage = (accordionId: string, type: string) => {
+    if (type === 'folder') {
+      router.push(`/dashboard/${workspaceId}/${accordionId}`)
+    }
+    if (type === 'file') {
+      router.push(`/dashboard/${workspaceId}/${folderId}/${accordionId}`)
+    }
+  }
+
+  const onchangeEmoji = async (selectedEmoji: string) => {
+    if (listType === 'folder') {
+    }
+  }
   //add a file
   //double click handler
   //blur
   //onChanges
   //move to trash
 
-  return <div>Dropdown</div>
+  return (
+    <AccordionItem
+      value={id}
+      className={listStyles}
+      onClick={(e) => {
+        e.stopPropagation()
+        navigateToPage(id, listType)
+      }}
+    >
+      <AccordionTrigger
+        id={listType}
+        className='p-2 
+        text-sm 
+        hover:no-underline 
+        dark:text-muted-foreground'
+        disabled={listType === 'file'}
+      >
+        <div className={groupIdentifies}>
+          <div
+            className='flex 
+          items-center 
+          justify-center 
+          gap-4 
+          overflow-hidden'
+          >
+            <div className='relative'>
+              <EmojiPicker getValue={onchangeEmoji}>{iconId}</EmojiPicker>
+            </div>
+          </div>
+        </div>
+      </AccordionTrigger>
+    </AccordionItem>
+  )
 }
 
 export default Dropdown
